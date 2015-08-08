d903822b8dcd675103b73a68ff4624efedd80e57
Jenn L <jenn.lazariuk@gmail.com>
2015-08-08 10:36:18 -1000

    created tests and trying to get to pass

--- challenge_8.js
+++ challenge_8.js
@@ -1,3 +1,29 @@
 // do work here
 
+module.exports = LargestProduct;
+
+
+function LargestProduct (n) {
+  this.productTotal = [];
+
+  for(var i = 0; i < n.length - 5; i++){
+    productTotal = n[i]
+  };
+
+}
+
+ LargestProduct.prototype.getLargestProductPosition = function() {
+  // body...
+};
+
+LargestProduct.prototype.getLargestProduct = function() {
+  // body...
+};
+
+
+
+
+
+
+
 // comment your code
